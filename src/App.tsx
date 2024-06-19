1849cfeab05f96ebed1fcd5cab51502133c5035e
ecali <78408765+ecali@users.noreply.github.com>
2024-06-19 12:00:30 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Footer} from "./components/footer";
+import {Header} from "./components/Header";
 
 function App() {
-  return (
-    <div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <div className={'max-w-[1000px] mx-auto'}>
+                <Header/>
+            </div>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <div className={'flex sm:h-[100px] h-[60px] items-center justify-center'}>
+            <p className={'sm:text-5xl text-xl'}>Simple Currency Converter</p>
+        </div>
+    )
+}
