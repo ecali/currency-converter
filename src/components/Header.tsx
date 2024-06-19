f488ea6fb9e2e937546393f32f7dcae655ce5cf4
ecali <78408765+ecali@users.noreply.github.com>
2024-06-19 12:13:53 +0200

    minor for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <div className={'flex sm:h-[100px] h-[60px] items-center justify-center'}>
+        <div className={'flex sm:h-[100px] h-[60px] items-center justify-center text-violet-800'}>
             <p className={'sm:text-5xl text-xl'}>Simple Currency Converter</p>
         </div>
     )
