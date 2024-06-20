3f1f85d1f489099f3a77dfbf5a84f6075398bc7b
ecali <78408765+ecali@users.noreply.github.com>
2024-06-20 14:36:49 +0200

    handle darkmode

--- src/components/currencySelect.tsx
+++ src/components/currencySelect.tsx
@@ -3,7 +3,6 @@ import {currencies} from "../utils/currency";
 
 export const CurrencySelect = (props: PropsCurrencySelector) => {
     return (
-
         <div className={'flex flex-col w-full'}>
             <label htmlFor="countries" className="block mb-2 text-sm font-medium text-primary">{props.label}</label>
             <select value={props.currency} disabled={props.disabled} onChange={(e) => props.setCurrency(e.target.value)} id="currencies" className="bg-primary border text-primary text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 border-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -11,22 +10,5 @@ export const CurrencySelect = (props: PropsCurrencySelector) => {
                 {currencies.map(currency => <option key={currency.code} value={currency.code}>({currency.code}) {currency.name}</option>)}
             </select>
         </div>
-
-
-
-        /*<FormControl fullWidth disabled={props.disabled}>
-            <InputLabel classes={'text-secondary'} id={props.label.toLowerCase().replaceAll(' ', '-')} >{props.label}</InputLabel>
-            <Select
-                labelId={props.label.toLowerCase().replaceAll(' ', '-')}
-                id={props.label.toLowerCase().replaceAll(' ', '-')}
-                value={props.currency}
-                label={props.label}
-                onChange={(e) => props.setCurrency(e.target.value)}
-            >
-                {currencies.map(currency => (
-                    <MenuItem className={props.currency === currency.code ? 'font-bold' : ''} key={currency.code} value={currency.code}>{currency.name} ({currency.code})</MenuItem>
-                ))}
-            </Select>
-        </FormControl>*/
     )
 }
--- src/components/topHeader.tsx
+++ src/components/topHeader.tsx
@@ -1,9 +1,15 @@
 import {Toggle} from "./Toggle";
+import {DarkMode, LightMode} from "@mui/icons-material";
 
 export const TopHeader = () => {
     return(
-        <div className={'w-full flex bg-violet h-[30px] justify-end'}>
-            <Toggle />
+        <div className={'w-full flex bg-violet h-[30px] justify-end px-3 items-center mb-3'}>
+            <div className={'flex gap-3'}>
+                <LightMode className={'text-yellow-400'} />
+                <Toggle />
+                <DarkMode className={'text-gray-600'} />
+            </div>
+
         </div>
     )
 }
